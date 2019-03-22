189ea26b394f6af98e3a1f7d2a5aff37c18eeafb
TorosKae <rus-toros-kae@ya.ru>
2019-03-22 17:54:52 +0300

    add comments

--- src/PlayerCompare.java
+++ src/PlayerCompare.java
@@ -9,7 +9,18 @@ public class PlayerCompare {
         int n = scan.nextInt();
 
         Player[] player = new Player[n];
-        Checker checker = new Checker();
+        Comparator<Player> checker = (Player a, Player b) -> {
+            int comScore = 0;
+            if (a.score>b.score){
+                comScore -=1;
+            } else if (a.score<b.score) {
+                comScore +=1;
+            }
+            int comName = a.name.compareTo(b.name);
+            if(comScore == 0){
+                return comName;
+            } else return comScore;
+        };//new Checker();
 
         for(int i = 0; i < n; i++){
             player[i] = new Player(scan.next(), scan.nextInt());
@@ -32,20 +43,20 @@ public class PlayerCompare {
         }
     }
 
-    static class Checker implements Comparator<Player> {
-        // complete this method
-
-        public int compare(Player a, Player b) {
-            int comScore = 0;
-            if (a.score>b.score){
-                comScore -=1;
-            } else if (a.score<b.score) {
-                comScore +=1;
-            }
-            int comName = a.name.compareTo(b.name);
-            if(comScore == 0){
-                return comName;
-            } else return comScore;
-        }
-    }
+//    static class Checker implements Comparator<Player> {
+//        // complete this method
+//
+//        public int compare(Player a, Player b) {
+//            int comScore = 0;
+//            if (a.score>b.score){
+//                comScore -=1;
+//            } else if (a.score<b.score) {
+//                comScore +=1;
+//            }
+//            int comName = a.name.compareTo(b.name);
+//            if(comScore == 0){
+//                return comName;
+//            } else return comScore;
+//        }
+//    }
 }
