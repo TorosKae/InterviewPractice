2904e768d9345a4bed1134bb2823cff23f4edb33
TorosKae <rus-toros-kae@ya.ru>
2019-04-05 17:19:10 +0300

    +LuckBalance

---
+++ src/LuckBalance.java
@@ -0,0 +1,84 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+/**
+ * https://www.hackerrank.com/challenges/luck-balance
+ * we have i conquests
+ * and k constraints to loose
+ * when you loose - you increase luck
+ * you can loose only k of the important conquests (1)
+ * when win luck decreased by value of conquest
+ * k = 3
+ * 5 1
+ * 2 1
+ * 1 1
+ * 8 1
+ * 10 0
+ * 5 0
+ * 10+5 (o importance) is guaranteed luck result + 15
+ * find k maximum values. it's 8, 5, 2. Result + 15
+ * decrease of luck after wining -1
+ * max Luck is 29
+ */
+public class LuckBalance {
+
+    // Complete the luckBalance function below.
+    static int luckBalance(int k, int[][] contests) {
+        int result = 0;
+        /*
+         [i][0] - value of luck in i conquest
+         [i][1] - importance of i conquest
+         when [i][1] = 0 res + luck [i][0]
+         when [i][1] = 1 and is add max Values + luck [i][0], k--
+            others i res--
+         k times we choose max of [][1]
+        */
+
+        for (int i = 0; i < contests.length; i++) {
+            if(contests[i][1]==0){
+                result+=contests[i][0];
+            } else {
+
+            }
+        }
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[][] contests = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            String[] contestsRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 2; j++) {
+                int contestsItem = Integer.parseInt(contestsRowItems[j]);
+                contests[i][j] = contestsItem;
+            }
+        }
+
+        int result = luckBalance(k, contests);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
